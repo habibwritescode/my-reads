9774d783baf9aaa35fead3461d21453e5b4ccfd2
Habib Afolabi <habibwritescode@gmail.com>
2020-07-11 14:01:52 +0100

    Refactor to handle errors to stop breaking app

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,7 +6,7 @@ const Book = (props) => {
         <li key={book.id}>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? (`url(${book.imageLinks.thumbnail})`) : '' }}></div>
                     <div className="book-shelf-changer">
                         <select>
                             <option value="move" disabled>Move to...</option>
@@ -18,9 +18,11 @@ const Book = (props) => {
                     </div>
                 </div>
                 <div className="book-title">{book.title}</div>
-                {book.authors.map(author => (
+                {book.authors ? book.authors.map(author => (
                     <div className="book-authors">{author}</div>
-                ))}
+                ))
+                    : ''
+                }
             </div>
         </li>
     )
