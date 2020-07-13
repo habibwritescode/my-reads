dcc5968ef9b4967d9121cd4a3dc46984b5be1061
Habib Afolabi <habibwritescode@gmail.com>
2020-07-13 15:17:03 +0100

    Run yarn build

---
+++ build/precache-manifest.cdc2d3b393fe38cbb7e1019d125fb605.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "21958a7d24e98382e0f2",
+    "url": "/static/css/main.aa90110d.chunk.css"
+  },
+  {
+    "revision": "21958a7d24e98382e0f2",
+    "url": "/static/js/main.21958a7d.chunk.js"
+  },
+  {
+    "revision": "0708eb5373d4cc62c9c5",
+    "url": "/static/js/1.0708eb53.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "6329f78a45ac5a7bf99a6106b3dd2a5a",
+    "url": "/index.html"
+  }
+];
